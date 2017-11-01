fbc375828a26799d5bab6c974098acf43adddfd4
rmsdev0 <rmsdev0@gmail.com>
2017-10-31 21:47:18 -0500

    Update taskrouter_dashboard.py

--- taskrouter_dashboard.py
+++ taskrouter_dashboard.py
@@ -65,7 +65,6 @@ def sync_taskrouter_statistics():
     url = 'https://sync.twilio.com/v1/Services/' + twilio_sync_service_id + '/Documents/' + sync_document
     response = requests.request("POST", url, data=new_data, auth=HTTPBasicAuth(twilio_account_sid, twilio_auth_token))
     print(response.text)
-    print 'task data'
     return (response.text)
 
 @app.route('/taskrouter_event', methods=['POST'])
